37149964e0ede0723283a8bec53a95ae91e888fc
gviteri <gviteri@ebi.ac.uk>
2018-09-04 11:30:46 +0100

    Adding JSAP configuration

--- src/main/java/org/reactome/server/tools/indexer/IconsMain.java
+++ src/main/java/org/reactome/server/tools/indexer/IconsMain.java
@@ -1,5 +1,6 @@
 package org.reactome.server.tools.indexer;
 
+import com.martiansoftware.jsap.*;
 import org.apache.solr.client.solrj.SolrClient;
 import org.reactome.server.tools.indexer.exception.IndexerException;
 import org.reactome.server.tools.indexer.icon.impl.IconIndexer;
@@ -15,10 +16,32 @@ import static org.reactome.server.tools.indexer.util.SolrUtility.getSolrClient;
 @Component
 public class IconsMain {
     private static final String DEF_SOLR_URL = "http://localhost:8983/solr";
+    private static final String DEF_SOLR_CORE = "reactome";
 
-    public static void main(String[] args) throws IndexerException {
-        SolrClient solrClient = getSolrClient("admin", "admin", DEF_SOLR_URL);
-        IconIndexer indexer = new IconIndexer(solrClient, "reactome-icon","/Users/reactome/Dev/icons/icon-lib/lib", "/Users/reactome/Dev/icons/ehld");
+
+    public static void main(String[] args) throws IndexerException, JSAPException {
+
+        SimpleJSAP jsap = new SimpleJSAP(Main.class.getName(), "A tool for generating a Solr Index.",
+                new Parameter[]{
+                        new FlaggedOption("solrUrl", JSAP.STRING_PARSER, DEF_SOLR_URL, JSAP.REQUIRED,           'a', "solrUrl",     "Url of the running Solr server"),
+                        new FlaggedOption("solrCore", JSAP.STRING_PARSER, DEF_SOLR_CORE, JSAP.REQUIRED,         'b', "solrCore",    "The Reactome solr core"),
+                        new FlaggedOption("solrUser", JSAP.STRING_PARSER, "admin", JSAP.NOT_REQUIRED,       'c', "solrUser",    "The Solr user"),
+                        new FlaggedOption("solrPw", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED,         'd', "solrPw",      "The Solr password"),
+                        new FlaggedOption("iconsDir", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.NOT_REQUIRED,   'e', "iconsDir",    "The Solr user"),
+                        new FlaggedOption("ehldDir", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.NOT_REQUIRED,    'f', "ehldDir",     "The Solr user"),
+                }
+        );
+
+        JSAPResult config = jsap.parse(args);
+        if (jsap.messagePrinted()) System.exit(1);
+
+        SolrClient solrClient = getSolrClient(config.getString("solrUser"), config.getString("solrPw"), config.getString("solrUrl"));
+
+        String solrCore = config.getString("solrCore");
+        String iconsDir = config.getString("iconsDir");
+        String ehldDir  = config.getString("ehldDir");
+
+        IconIndexer indexer = new IconIndexer(solrClient, solrCore, iconsDir, ehldDir);
         indexer.indexIcons();
         closeSolrServer(solrClient);
     }
