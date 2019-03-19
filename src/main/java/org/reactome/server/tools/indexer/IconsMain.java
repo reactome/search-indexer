398a163f098e25d31120896d5c99a6157609820e
gviteri <gviteri@ebi.ac.uk>
2019-03-19 16:15:12 +0000

    adding icon exporter

--- src/main/java/org/reactome/server/tools/indexer/IconsMain.java
+++ src/main/java/org/reactome/server/tools/indexer/IconsMain.java
@@ -3,6 +3,7 @@ package org.reactome.server.tools.indexer;
 import com.martiansoftware.jsap.*;
 import org.apache.solr.client.solrj.SolrClient;
 import org.reactome.server.tools.indexer.exception.IndexerException;
+import org.reactome.server.tools.indexer.icon.exporter.IconsExporter;
 import org.reactome.server.tools.indexer.icon.impl.IconIndexer;
 import org.springframework.stereotype.Component;
 
@@ -18,17 +19,17 @@ public class IconsMain {
     private static final String DEF_SOLR_URL = "http://localhost:8983/solr";
     private static final String DEF_SOLR_CORE = "reactome";
 
-
     public static void main(String[] args) throws IndexerException, JSAPException {
 
         SimpleJSAP jsap = new SimpleJSAP(IconsMain.class.getName(), "A tool for generating a Solr Index for Icons.",
                 new Parameter[]{
-                        new FlaggedOption("solrUrl", JSAP.STRING_PARSER, DEF_SOLR_URL, JSAP.REQUIRED,           'a', "solrUrl",     "Url of the running Solr server"),
-                        new FlaggedOption("solrCore", JSAP.STRING_PARSER, DEF_SOLR_CORE, JSAP.REQUIRED,         'b', "solrCore",    "The Reactome solr core"),
-                        new FlaggedOption("solrUser", JSAP.STRING_PARSER, "admin", JSAP.NOT_REQUIRED,       'c', "solrUser",    "The Solr user"),
-                        new FlaggedOption("solrPw", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED,         'd', "solrPw",      "The Solr password"),
-                        new FlaggedOption("iconsDir", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.NOT_REQUIRED,   'e', "iconsDir",    "The Solr user"),
-                        new FlaggedOption("ehldDir", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.NOT_REQUIRED,    'f', "ehldDir",     "The Solr user"),
+                        new FlaggedOption("solrUrl",    JSAP.STRING_PARSER, DEF_SOLR_URL,       JSAP.REQUIRED,      'a', "solrUrl",     "Url of the running Solr server"),
+                        new FlaggedOption("solrCore",   JSAP.STRING_PARSER, DEF_SOLR_CORE,      JSAP.REQUIRED,      'b', "solrCore",    "The Reactome solr core"),
+                        new FlaggedOption("solrUser",   JSAP.STRING_PARSER, "admin",        JSAP.NOT_REQUIRED,  'c', "solrUser",    "The Solr user"),
+                        new FlaggedOption("solrPw",     JSAP.STRING_PARSER, JSAP.NO_DEFAULT,    JSAP.REQUIRED,      'd', "solrPw",      "The Solr password"),
+                        new FlaggedOption("iconsDir",   JSAP.STRING_PARSER, JSAP.NO_DEFAULT,    JSAP.NOT_REQUIRED,  'e', "iconsDir",    "The Solr user"),
+                        new FlaggedOption("ehldDir",    JSAP.STRING_PARSER, JSAP.NO_DEFAULT,    JSAP.NOT_REQUIRED,  'f', "ehldDir",     "The Solr user"),
+                        new FlaggedOption("outputDir",  JSAP.STRING_PARSER, ".",            JSAP.NOT_REQUIRED,  'g', "outputDir",   "The icons mapping file output directory")
                 }
         );
 
@@ -41,8 +42,11 @@ public class IconsMain {
         String iconsDir = config.getString("iconsDir");
         String ehldDir  = config.getString("ehldDir");
 
-        IconIndexer indexer = new IconIndexer(solrClient, solrCore, iconsDir, ehldDir);
-        indexer.indexIcons();
+        IconIndexer iconIndexer = new IconIndexer(solrClient, solrCore, iconsDir, ehldDir);
+        iconIndexer.indexIcons();
         closeSolrServer(solrClient);
+
+        IconsExporter tsvWriter = new IconsExporter(solrClient, config.getString("solrCore"));
+        tsvWriter.write(config.getString("outputDir"));
     }
 }
