5bd9078666150ea848ea52d7ab47ca2f25214d08
Guilherme Viteri <gviteri@ebi.ac.uk>
2021-07-05 11:28:17 +0100

    CLI Implementation

--- src/main/java/org/reactome/server/tools/indexer/config/IndexerNeo4jConfig.java
+++ src/main/java/org/reactome/server/tools/indexer/config/IndexerNeo4jConfig.java
@@ -1,23 +1,31 @@
 package org.reactome.server.tools.indexer.config;
 
+import org.aspectj.lang.Aspects;
+import org.reactome.server.graph.aop.LazyFetchAspect;
 import org.reactome.server.graph.config.GraphCoreNeo4jConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
-/**
- * @author Guilherme S Viteri <gviteri@ebi.ac.uk>
- */
 @Configuration
 @ComponentScan(basePackages = {"org.reactome.server"})
+@EntityScan(basePackages = {"org.reactome.server.graph.domain.model"})
 @EnableTransactionManagement
 @EnableNeo4jRepositories(basePackages = {"org.reactome.server.graph.repository"})
+@EnableSpringConfigured
 public class IndexerNeo4jConfig extends GraphCoreNeo4jConfig {
-    private static final Logger logger = LoggerFactory.getLogger("importLogger");
 
-
+    /**
+     * This is needed to get hold of the instance of the aspect which is created outside of the spring container,
+     * and make it available for autowiring.
+     */
+    @Bean
+    public LazyFetchAspect lazyFetchAspect() {
+        return Aspects.aspectOf(LazyFetchAspect.class);
+    }
 }
