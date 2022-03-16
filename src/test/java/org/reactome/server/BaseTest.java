6581649d5f4abed3d0618cf9ef71294e738fab43
EliotRagueneau <159753.AZErty>
2022-03-16 12:01:44 +0000

    :sparkles: Add indexer test to easily work on documents output

---
+++ src/test/java/org/reactome/server/BaseTest.java
@@ -0,0 +1,46 @@
+package org.reactome.server;
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.reactome.server.graph.aop.LazyFetchAspect;
+import org.reactome.server.graph.config.GraphCoreNeo4jConfig;
+import org.reactome.server.graph.service.GeneralService;
+import org.reactome.server.graph.utils.ReactomeGraphCore;
+import org.reactome.server.tools.indexer.config.IndexerNeo4jConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ContextConfiguration(classes = {IndexerNeo4jConfig.class})
+@ComponentScan(basePackages = "org.reactome.server")
+@ExtendWith(SpringExtension.class)
+public abstract class BaseTest {
+
+    protected final Logger logger = LoggerFactory.getLogger("testLogger");
+
+
+    @Autowired
+    protected LazyFetchAspect lazyFetchAspect;
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeAll
+    public static void setUpStatic(@Value("${spring.neo4j.uri}") String uri,
+                                   @Value("${spring.neo4j.authentication.username}") String user,
+                                   @Value("${spring.neo4j.authentication.password}") String pass) {
+        ReactomeGraphCore.initialise(uri, user, pass);
+    }
+
+}
---
+++ src/test/java/org/reactome/server/tools/indexer/impl/IndexerTest.java
@@ -0,0 +1,34 @@
+package org.reactome.server.tools.indexer.impl;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.reactome.server.BaseTest;
+import org.reactome.server.tools.indexer.model.IndexDocument;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+class IndexerTest extends BaseTest {
+
+    @Autowired
+    DocumentBuilder documentBuilder;
+    private static ObjectMapper objectMapper;
+
+    @BeforeAll
+    static void setUp() {
+        objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+    }
+
+    @Test
+    void indexDrug() throws JsonProcessingException {
+
+        IndexDocument document = documentBuilder.createSolrDocument(9649889L);
+
+        logger.info(objectMapper.writeValueAsString(document));
+    }
+}
